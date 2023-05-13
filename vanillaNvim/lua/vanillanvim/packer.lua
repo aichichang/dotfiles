496f4c6f12d34f6a28e6abcbd52c5df2bb12ca2f
aichichang <aichi.chang@nandos.co.uk>
2023-05-13 22:02:21 +0100

    Add neo tree to nvim config

--- vanillaNvim/lua/vanillanvim/packer.lua
+++ vanillaNvim/lua/vanillanvim/packer.lua
@@ -2,6 +2,7 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return require("packer").startup(function(use)
   -- Packer can manage itself
@@ -74,6 +75,16 @@ return require("packer").startup(function(use)
       -- Snippets
       { "L3MON4D3/LuaSnip" }, -- Required
       { "rafamadriz/friendly-snippets" }, -- Optional
+    },
+  }
+
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
     },
   }
 end)
